62dc5b18110ffbf00545b51a78036f2220ed8b7e
Jonas Thiem <jonthiem@gmail.com>
2019-07-03 02:57:16 +0200

    add support for dark rising

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ else
     local bit = require("bit")
 
     -- Data
-    local addresses = {0x020244EC, 0x02024284}
+    local addresses = {0x020244EC, 0x02024284, 0x02024284}
     local partyAddress =  addresses[game]
 
     local numberedSprites
@@ -22,6 +22,57 @@ else
         vba.print("Using named sprites.")
         numberedSprites = false
     end
+
+    local darkRisingDB = {
+        "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
+        "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Venipede", "Whirlipede",
+        "Scolipede", "Pidgey", "Pidgeotto", "Pidgeot", "Blitzle", "Zebstrika", "Rufflet", "Braviary",
+        "Ekans", "Arbok", "Pikachu", "Raichu", "Zorua", "Zoroark", "Solosis", "Duosion", "Reuniclus",
+        "Litwick", "Lampent", "Chandelure", "Snover", "Abomasnow", "Vulpix", "Ninetales", "Yamask",
+        "Cofagrigus", "Zubat", "Golbat", "Gothita", "Gothorita", "Gothitelle", "Patrat", "Watchog",
+        "Minccino", "Cinccino", "Drilbur", "Excadrill", "Mienfoo", "Mienshao", "Hippopotas", 
+        "Hippowdon", "Pawniard", "Bisharp", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath",
+        "Abra", "Kadabra", "Alakazam",  "Machop", "Machoke", "Machamp", "Starly", "Staravia", "Staraptor",
+        "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro",
+        "Magnemite", "Magneton", "Axew", "Fraxure", "Haxorus", "Dwebble", "Crustle", "Croagunk", "Toxicroak",
+        "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Bronzor", "Bronzong", "Scraggy",
+        "Scrafty", "Riolu", "Lucario", "Exeggcute", "Exeggutor", "Darumaka", "Darmanitan", "Hitmonlee", 
+        "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", 
+        "Horsea", "Seadra", "Ducklett", "Swanna", "Staryu", "Starmie", "Spiritomb", "Scyther", "Jynx", 
+        "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados","Lapras", "Ditto", "Eevee", "Vaporeon", 
+        "Jolteon", "Flareon", "Porygon", "Tirtouga", "Carracosta", "Archen", "Archeops", "Aerodactyl", "Snorlax",
+        "Alomomola", "Durant", "Zen Mode", "Dratini", "Dragonair", "Dragonite", "Audino", "Mew",
+
+        "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion", "Totodile", "Croconaw", 
+        "Feraligatr", "Munna", "Musharna", "Buizel", "Floatzel", "Joltik", "Galvantula", "Spinarak", "Ariados", 
+        "Crobat", "Chinchou", "Lanturn", "Pichu", "Shellos", "Gastrodon", "Togepi", "Togetic", "Drifloon",
+        "Driftblim", "Timburr", "Gurdurr", "Conkeldurr", "Emolga", "Bouffalant", "Throh", "Sawk", "Politoed",
+        "Roggenrola", "Boldore", "Gigalith", "Aipom", "Cottonee", "Whimsicott", "Yanma", "Frillish",
+        "Jellicent", "Espeon", "Umbreon", "Murkrow", "Slowking", "Misdreavus", "Unown", "Wobbuffet", "Gallade",
+        "Pineco", "Forretress", "Rhyperior", "Gligar", "Steelix", "Skorupi", "Drapion", "Gliscor", "Scizor",
+        "Tangrowth", "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Cubchoo", "Beartic", "Swinub", "Piloswine",
+        "Gible", "Gabite", "Garchomp", "Delibird", "Togekiss", "Skarmory", "Houndour", "Houndoom", "Kingdra", 
+        "Phanpy", "Donphan", "Porygon2", "Munchlax", "Druddigon", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", 
+        "Magby", "Miltank", "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia",
+        "Ho-Oh", "Celebi",
+
+        "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert",
+        "Turtwig", "Grotle", "Torterra", "Tepig", "Pignite", "Emboar", "Snivy", "Servine", "Serperior", "Lotad",
+        "Lombre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow", "Eelektrik", "Eelektross",
+        "Ralts", "Kirlia", "Gardevoir", "Piplup", "Prinplup", "Shroomish", "Breloom", "Slakoth", "Vigoroth", 
+        "Slaking", "Yanmega", "Ambipom", "Shedinja", "Golett", "Golurk", "Dusknoir", "Karrablast", "Escavalier",
+        "Honchkrow","Nosepass", "Sandile", "Krokorok", "Sableye", "Roserade", "Aron", "Lairon", "Aggron",
+        "Porygon-Z", "Magnezone", "Shelmet", "Accelgor", "Larvesta", "Volcarona", "Lickilicky", "Probopass",
+        "Roselia", "Chimchar", "Monferno", "Carvanha", "Sharpedo", "Empoleon", "Mamoswine", "Electivire",
+        "Magmortar", "Torkoal", "Spoink", "Grumpig", "Tynamo", "Trapinch", "Vibrava", "Flygon", "Cacnea", 
+        "Cacturne", "Swablu", "Altaria", "Weavile", "Froslass", "Glaceon", "Leafeon", "Oshawott", "Dewott",
+        "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Shieldon", "Bastiodon", "Cranidos", "Rampardos",
+        "Feebas", "Milotic", "Castform", "Krookodile", "Ferroseed", "Ferrothorn", "Duskull", "Dusclops",
+        "Infernape", "Shaymin", "Absol", "Wynaut", "Snorunt", "Glalie", "Spheal", "Sealeo", "Walrein",
+        "Deino", "Zweilous", "Hydreigon", "Mismagius", "Samurott", "Bagon", "Shelgon", "Salamence", "Beldum",
+        "Metang", "Metagross", "Tornadus", "Thundurus", "Landorus", "Victini", "Keldeo", "Kyogre", "Groudon",
+        "Rayquaza", "Jirachi", "Deoxys"
+    }
 
     local pkmNameDB = {"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard",
         "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree",
@@ -101,9 +152,11 @@ else
     }
 
     -- code
+    if game == 3 then pkmNameDB = darkRisingDB end
 
     local prevParty = {0, 0, 0, 0, 0, 0}
     local party = {0, 0, 0, 0, 0, 0}
+    local originalParty = {0, 0, 0, 0, 0, 0}
 
     local function getbits(a,b,d)
         return bit.rshift(a,b) % bit.lshift(1,d)
@@ -134,15 +187,20 @@ else
                 party[i] = speci
             end     
         end
+        if originalParty[1] == 0 then copyArray(party, originalParty) end
     end
 
+    local firstRead = true
     local function didPartyChange()
-        if party[1] == 0 then return {false} end
+        if firstRead then
+            return {true, true, true, true, true, true, true}
+        end
+        if party[1] == 0 then return {true} end
         
         local r = {false, false, false, false, false, false, false}
         
         for i = 1, 6, 1 do
-            if prevParty[i] ~= party[i] then
+            if party[i] ~= originalParty[i] then
                 r[1] = true
                 r[i + 1] = true 
             end
@@ -159,14 +217,22 @@ else
         end
     end
 
+    local changeTimer = 0
     local function update()
         readParty()
 
         local changeMap = didPartyChange()
         if changeMap[1] then
+            changeTimer = changeTimer + 1
+        else
+            changeTimer = 0
+        end
+
+        if changeTimer == 120 or firstRead then
+            copyArray(party, originalParty)
             for i = 1, 6, 1 do
                 if changeMap[i + 1] then
-                    local id = party[i]
+                    local id = originalParty[i]
                     if id <= 0x19c then
                         local pngPath
                         if id == 0x19C then
@@ -176,7 +242,7 @@ else
                         end
                         local newPNG = io.open(pngPath, "rb")
                         if newPNG == nil then
-                            vba.print(getPNGPath(party[i]) .. " is missing.")
+                            vba.print(getPNGPath(originalParty[i]) .. " is missing.")
                         else
                             local newData = newPNG:read("*a")
                             newPNG:flush()
@@ -188,6 +254,8 @@ else
                     end
                 end
             end
+            changeTimer = 0
+            firstRead = false
         end
     end
 
--- readme.md
+++ readme.md
@@ -1,24 +1,27 @@
 # g3pd - Gen 3 Pokemon Display
 
 ## What?
+
 This script allows streamers to display their current party to their viewers.
 
 ## Setup
+
 1. Get [Viusal Boy Advanced Rerecording](https://github.com/TASVideos/vba-rerecording/releases) if you don't already use it.
 
 2. [Get the latest version of g3pd](https://github.com/PeachIceTea/g3pd/archive/master.zip).
 
-3. Get sprites you like. I used [these](https://veekun.com/dex/downloads). Also, there needs to be a 0.png that matches the dimensions of the other sprites but is emtpy. For eggs there needs to be a egg.png.
+3. Get sprites you like. I used [these](https://veekun.com/dex/downloads). Sprites have the following additional requirements:
+
+    3.1 A "0.png" that matches the dimensions of the other sprites, but is emtpy.
+
+    3.2 An "egg.png" for, well, eggs.
 
 4. Extract g3pd into its own folder.
 
 5. Extract the sprites into the "sprites" folder.
 
-6. Use a text editor to set the game in the "SetGameInHere.lua" file. 1 for Emerald and 2 for FireRed/LeafGreen.
+6. Use a text editor to set the game in the "SetGameInHere.lua" file. 1 for Emerald, 2 for FireRed/LeafGreen, and 3 for Dark Rising.
 
-7. Run VBA and when the game is loaded. Load the script from Tools/Lua Scripting/New Lua Script Window...
+7. Run VBA and when the game is loaded, load the script from Tools/Lua Scripting/New Lua Script Window...
 
 8. Add the png files in the "party" folder to your OBS overlay.
-
-## Note
-Currently only Pokemon Emerald (USA) is supported. Please open an issue if you need a version for other games.
